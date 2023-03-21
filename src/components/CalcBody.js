db0e6c61b660d9fc22e755cc4b932aba5b04928b
shubham <shubham.bhoyar077@gmail.com>
2023-03-21 18:52:16 +0530

    Add onClick event handeler

--- src/components/CalcBody.js
+++ src/components/CalcBody.js
@@ -1,11 +1,21 @@
-function CalcBody() {
+import PropTypes from 'prop-types';
+
+function CalcBody({ handelClick }) {
   const clacFunc = ['AC', '+/-', '%', '÷',
     7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
   return (
     <div className="calc-body">
-      {clacFunc.map((element) => (<button type="button" key={element}>{element}</button>))}
+      {clacFunc.map((element) => (
+        <button value={element} type="button" key={element} onClick={handelClick}>
+          {element}
+        </button>
+      ))}
     </div>
   );
 }
+
+CalcBody.propTypes = {
+  handelClick: PropTypes.func.isRequired,
+};
 
 export default CalcBody;
