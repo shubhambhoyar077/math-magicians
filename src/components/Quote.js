310b1d68a071605dbfa3a99887215991371dfac9
shubham <shubham.bhoyar077@gmail.com>
2023-03-22 18:11:33 +0530

    Create quote componenet and fetching data from api

---
+++ src/components/Quote.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+function Quote() {
+  const [quote, setQuote] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errMessage, setErrMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+        headers: { 'X-Api-Key': process.env.REACT_APP_QUOTE_API_KEY },
+        contentType: 'application/json',
+      });
+      if (!response.ok) {
+        throw Error('could not fetch data.');
+      }
+      return response.json();
+    };
+    fetchQuote()
+      .then((data) => {
+        setQuote(data[0].quote);
+        setIsLoading(false);
+        setErrMessage(null);
+      })
+      .catch((err) => {
+        setQuote(null);
+        setIsLoading(false);
+        setErrMessage(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="quote">
+      {isLoading && <div>Loading...</div>}
+      {quote && <div>{quote}</div>}
+      {errMessage && <div>{errMessage}</div>}
+    </div>
+  );
+}
+
+export default Quote;
